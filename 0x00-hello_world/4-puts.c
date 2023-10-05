f644c6d122c03b736e0909060e7f947189ac319d
adelajahalima <adelajahalima@gmail.com>
2023-10-05 16:50:26 -0700

    Edited 4-puts file to contain the answer to task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
