9872e1c965e5f3d7df71afcb83b84cebf43b780e
adelajahalima <adelajahalima@gmail.com>
2023-10-07 07:32:34 -0700

    Added 6-print_number.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prog entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
