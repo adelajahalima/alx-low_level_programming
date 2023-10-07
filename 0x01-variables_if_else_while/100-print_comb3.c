b93f2fee9cdaabd34dc13eb462286183966f05e6
adelajahalima <adelajahalima@gmail.com>
2023-10-07 15:42:45 -0700

    Edited 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = 0; a <= 9; tens++)
+	for (tens = 0; tens <= 9; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
 			{
-				putchar(tens)
-				putchar(ones)
+				putchar(tens);
+				putchar(ones);
 				if (!(ones == '9' && tens == '8'))
 				{
-					 putchar(',');
-					 putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
