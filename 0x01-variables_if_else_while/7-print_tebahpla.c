ac082ec52986112c8efc5245203d0d00b5b1ab7f
adelajahalima <adelajahalima@gmail.com>
2023-10-07 07:50:49 -0700

    Added a file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prog entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
