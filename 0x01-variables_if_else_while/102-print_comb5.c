491abaa632d379178340c496b515e38679e99f67
adelajahalima <adelajahalima@gmail.com>
2023-10-07 17:55:29 -0700

    Edited file 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48));
-				putchar((i % 10) + 48));
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) + 48));
-				putchar((j % 10) + 48));
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 && j != 99)
 				{
 					putchar(',');
