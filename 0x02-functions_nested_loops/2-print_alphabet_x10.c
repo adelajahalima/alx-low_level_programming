1a82e5d3a25ca7e18c8809d2c60fa1daaff595e2
adelajahalima <adelajahalima@gmail.com>
2023-10-11 01:24:34 -0700

    Edited file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
