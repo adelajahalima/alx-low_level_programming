7b80bd96408ce82a575a96bcc8110a5852488f09
adelajahalima <adelajahalima@gmail.com>
2024-02-20 03:22:50 +0100

    Added file 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "hash_tables.h"
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
